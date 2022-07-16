5a6383bcf415f6e42df9b2227349ef9bb7474605
Gana <16054cn021@gmail.com>
2022-07-16 23:19:22 +0530

    ignore FetchSong

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import FetchSong from "./FetchSong";
-import reportWebVitals from "./reportWebVitals";
 
-FetchSong();
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
